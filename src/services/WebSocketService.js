cd9261cf732b8a066228635d9bc87a0bd72aa8fd
Saray Alieth Mendivelso Gonzalez <111905625+saraygonm@users.noreply.github.com>
2024-12-11 18:45:28 -0500

    /ws

--- src/services/WebSocketService.js
+++ src/services/WebSocketService.js
@@ -5,7 +5,7 @@ let client;
 
 export function connect(onConnectCallback) {
     client = new Client({
-        webSocketFactory: () => new SockJS('https://mindracers2-dpa2hqfmbugggth5.canadacentral-01.azurewebsites.net'),
+        webSocketFactory: () => new SockJS('https://mindracers2-dpa2hqfmbugggth5.canadacentral-01.azurewebsites.net/ws'),
         onConnect: () => {
             console.log('Connected');
             onConnectCallback();
