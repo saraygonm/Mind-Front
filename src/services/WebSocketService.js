9812fb9289780add1dc817446aa24afb67da29bc
Saray Alieth Mendivelso Gonzalez <111905625+saraygonm@users.noreply.github.com>
2024-12-11 20:58:56 -0500

    Update WebSocketService.js

--- src/services/WebSocketService.js
+++ src/services/WebSocketService.js
@@ -6,7 +6,7 @@ let client;
 export function connect(onConnectCallback) {
     console.log("entry connect func");
     client = new Client({
-        webSocketFactory: () => new SockJS('https://mindracers2-dpa2hqfmbugggth5.canadacentral-01.azurewebsites.net/ws'),
+        webSocketFactory: () => new SockJS('https://mindracers2-dpa2hqfmbugggth5.canadacentral-01.azurewebsites.net'),
         onConnect: () => {
             console.log('Connected');
             onConnectCallback();
